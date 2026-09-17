506cb8e2b5df281370ed895e261e50f7eba736bf
Emiliano Acuña <A01254706@tec.mx>
2026-09-17 12:35:30 -0700

    FFGL SDK v2.2 + EventTimer plugin scaffolding
    - Cloned FFGL SDK from resolume/ffgl at tag v2.2 (compatible with Resolume 7.0.3+)
    - Updated all .vcxproj files: PlatformToolset v142->v143, Windows SDK 10.0.18362->10.0.26100
    - Compiled Gradient example plugin in Debug (validates toolchain)
    - Created EventTimer plugin (Source type) based on Gradients template:
      - FFGLEventTimer.h/cpp with PluginInfo (ID: RS04, author: rodrmilano00)
      - EventTimer.vcxproj + .filters with post-build copy to Resolume plugins folder
      - Added to FFGLPlugins.sln under sources folder
    - Post-build event copies DLL to C:\Users\luis1\Documents\Resolume Arena\Extra Effects\
    - Verified: EventTimer.dll compiles and copies successfully

---
+++ ffgl/source/plugins/EventTimer/FFGLEventTimer.cpp
@@ -0,0 +1,115 @@
+#include "FFGLEventTimer.h"
+using namespace ffglex;
+
+enum ParamType : FFUInt32
+{
+	PT_RED,
+	PT_GREEN,
+	PT_BLUE,
+	PT_ALPHA
+};
+
+static CFFGLPluginInfo PluginInfo(
+	PluginFactory< FFGLEventTimer >,  // Create method
+	"RS04",                           // Plugin unique ID
+	"Event Timer",                    // Plugin name
+	2,                                // API major version number
+	1,                                // API minor version number
+	1,                                // Plugin major version number
+	000,                              // Plugin minor version number
+	FF_SOURCE,                        // Plugin type
+	"Customizable event timer/countdown for live performances", // Plugin description
+	"rodrmilano00"                    // Author
+);
+
+static const char vertexShaderCode[] = R"(#version 410 core
+layout( location = 0 ) in vec4 vPosition;
+layout( location = 1 ) in vec2 vUV;
+
+out vec2 uv;
+
+void main()
+{
+	gl_Position = vPosition;
+	uv = vUV;
+}
+)";
+
+static const char fragmentShaderCode[] = R"(#version 410 core
+uniform vec4 Color;
+
+in vec2 uv;
+
+out vec4 fragColor;
+
+void main()
+{
+	fragColor = Color;
+}
+)";
+
+FFGLEventTimer::FFGLEventTimer() :
+	colorLocation( -1 )
+{
+	// Input properties
+	SetMinInputs( 0 );
+	SetMaxInputs( 0 );
+
+	// Parameters
+	SetParamInfof( PT_RED, "Red", FF_TYPE_RED );
+	SetParamInfof( PT_GREEN, "Green", FF_TYPE_GREEN );
+	SetParamInfof( PT_BLUE, "Blue", FF_TYPE_BLUE );
+	SetParamInfof( PT_ALPHA, "Alpha", FF_TYPE_ALPHA );
+
+	FFGLLog::LogToHost( "Created Event Timer" );
+}
+
+FFResult FFGLEventTimer::InitGL( const FFGLViewportStruct* vp )
+{
+	if( !shader.Compile( vertexShaderCode, fragmentShaderCode ) )
+	{
+		DeInitGL();
+		return FF_FAIL;
+	}
+	if( !quad.Initialise() )
+	{
+		DeInitGL();
+		return FF_FAIL;
+	}
+
+	ScopedShaderBinding shaderBinding( shader.GetGLID() );
+	colorLocation = shader.FindUniform( "Color" );
+
+	return CFFGLPlugin::InitGL( vp );
+}
+
+FFResult FFGLEventTimer::ProcessOpenGL( ProcessOpenGLStruct* pGL )
+{
+	ScopedShaderBinding shaderBinding( shader.GetGLID() );
+	glUniform4f( colorLocation, 0.2f, 0.2f, 0.2f, 1.0f );
+
+	quad.Draw();
+
+	return FF_SUCCESS;
+}
+
+FFResult FFGLEventTimer::DeInitGL()
+{
+	shader.FreeGLResources();
+	quad.Release();
+	colorLocation = -1;
+
+	return FF_SUCCESS;
+}
+
+FFResult FFGLEventTimer::SetFloatParameter( unsigned int dwIndex, float value )
+{
+	// TODO: Implement parameter handling
+	return FF_SUCCESS;
+}
+
+float FFGLEventTimer::GetFloatParameter( unsigned int index )
+{
+	// TODO: Implement parameter getting
+	return 0.0f;
+}
